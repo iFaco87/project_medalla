e42e78a1e8e0002b288ed250369dd6b0b4d9386f
Freddian Ciares <freddian.ciares@gustazos.comm>
2025-04-01 14:30:46 -0400

    Elastic IP for the EC2

--- uat/ec2.tf
+++ uat/ec2.tf
@@ -1,5 +1,10 @@
 module "ec2" {
-  source    = "git::ssh://git@github.com/iFaco87/terraform-child-module.git//ec2?ref=v0.0.3"
+  source    = "git::ssh://git@github.com/iFaco87/terraform-child-module.git//ec2?ref=v0.0.7"
   instances = local.instances
   keypairs  = var.keypairs
 }
+
+resource "aws_eip" "uat-elp" {
+  domain = "vpc"
+}
+
