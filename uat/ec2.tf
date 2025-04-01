2facfe057d2d1d596efc1a5a315f672cb58be4af
Freddian Ciares <freddian.ciares@gustazos.comm>
2025-04-01 13:12:17 -0400

    Adding Versioning

--- uat/ec2.tf
+++ uat/ec2.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source    = "git@github.com:iFaco87/terraform-child-module.git/ec2"
+  source    = "git@github.com:iFaco87/terraform-child-module.git/ec2?ref=v0.0.1"
   instances = local.instances
   keypairs  = var.keypairs
 }
