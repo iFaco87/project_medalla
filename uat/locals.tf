f45536bea091dd1a64133046038629ba2af2f0ba
Freddian Ciares <freddian.ciares@gustazos.comm>
2025-04-01 14:12:21 -0400

    Rollback Security Group ID

--- uat/locals.tf
+++ uat/locals.tf
@@ -1,10 +1,11 @@
 locals {
+  security_group_id = module.sg_uat.id
   instances = {
     "uat" = {
       key_name                    = "mj"
       instance_type               = "t3.2xlarge"
       subnet_id                   = module.network.public_subnet_ids[0]
-      security_group_ids          = [module.security_group_id]
+      security_group_ids          = [local.security_group_id]
       associate_public_ip_address = true
       monitoring                  = false
       volume_size                 = 500
