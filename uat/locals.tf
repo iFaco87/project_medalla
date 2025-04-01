01f9901d25da14654e753c0039882daa178f16ba
Freddian Ciares <freddian.ciares@gustazos.comm>
2025-04-01 14:10:03 -0400

    Using US-East-1 AMI

--- uat/locals.tf
+++ uat/locals.tf
@@ -1,12 +1,10 @@
 locals {
-  security_group_id = module.sg_uat.id
-  public_subnet_ids = module.network.public_subnet_ids
   instances = {
     "uat" = {
       key_name                    = "mj"
       instance_type               = "t3.2xlarge"
-      subnet_id                   = local.public_subnet_ids[0]
-      security_group_ids          = [local.security_group_id]
+      subnet_id                   = module.network.public_subnet_ids[0]
+      security_group_ids          = [module.security_group_id]
       associate_public_ip_address = true
       monitoring                  = false
       volume_size                 = 500
